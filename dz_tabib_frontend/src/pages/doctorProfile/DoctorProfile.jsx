b3005974415c71b55c1b78d553a5781755db426d
ikram <i_manaa@estin.dz>
2024-12-06 20:10:06 +0100

    react,django setup

---
+++ dz_tabib_frontend/src/pages/doctorProfile/DoctorProfile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const DoctorProfile = () => {
+  return( 
+  <h1>doctors profile page </h1>
+  );
+};
---
+++ dz_tabib_frontend/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const HomePage = () => {
+  return (
+  <h1> home page </h1>
+);
+};
+
