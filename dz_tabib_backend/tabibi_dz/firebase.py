5e79952842a84fcb320ff0a473ff8bf03a87f557
djihad-elhak <d_belkihr@estin.dz>
2025-01-26 12:57:52 +0100

    Update .gitignore to exclude node_modules

--- dz_tabib_backend/tabibi_dz/firebase.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-def initialize_firebase():
-    cred = credentials.Certificate('firebase_service_account.json')
-    firebase_admin.initialize_app(cred)
-
-# Call the function to initialize Firebase in your settings or views
-initialize_firebase()
