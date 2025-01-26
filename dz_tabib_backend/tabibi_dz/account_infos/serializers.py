b2e85ff39de7795a0f65c79fbb1f21c742113e33
djihad-elhak <d_belkihr@estin.dz>
2025-01-26 12:57:52 +0100

    Update .gitignore to exclude node_modules

--- dz_tabib_backend/tabibi_dz/account_infos/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class ChangePasswordSerializer(serializers.Serializer):
-    password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
--- dz_tabib_backend/tabibi_dz/search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SearchDoctorsAPIView
-
-urlpatterns = [
-    path('search/doctors/', SearchDoctorsAPIView.as_view(), name='search_doctors'),
-]
