84981bab871a2f63dbf0f2202b7d76de9f1447ff
Djihad El Hak Belkhir <165265630+djihad-elhak@users.noreply.github.com>
2025-01-26 10:45:51 +0100

    Delete dz_tabib directory

--- dz_tabib/appointments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('doctor/<int:doctor_id>/available_slots/<str:date>/', views.get_available_slots, name='get_available_slots'),
-    path('book_appointment/', views.book_appointment, name='book_appointment'),
-    path('cancel_appointment/', views.cancel_appointment, name='cancel_appointment'),
-    path('appointment_history/<int:patient_card_id>/', views.get_appointment_history, name='get_appointment_history'),
-]
