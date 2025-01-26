2883b9dc4211d3045a311420e62fe0d247316de0
ikrammanaa <161580522+ikrammanaa@users.noreply.github.com>
2025-01-26 17:17:31 +0100

    Update vite.config.js

--- dz_tabib_frontend/vite.config.js
+++ dz_tabib_frontend/vite.config.js
@@ -5,14 +5,22 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-calendar'], // Add this line
+//     },
+//   },
+// });
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Ensures relative paths for assets
   build: {
-    rollupOptions: {
-      external: ['react-calendar'], // Add this line
-    },
+    chunkSizeWarningLimit: 1000, // Optional: Suppress chunk size warning
   },
 });
